77917ecae91de7f632c2a210bab87b1eef634e55
Chris Barker <Chris.Barker@noaa.gov>
2016-07-16 11:13:51 -0500

    added a place for the gridfu package for testing.

---
+++ gridfu/__init__.py
@@ -0,0 +1,6 @@
+# gridfu package
+# package for providing a unified API to various grid types used by oceangraphic
+# models (and maybe others)
+
+# should provide interface to pyugrid, pysgrid, ....
+
